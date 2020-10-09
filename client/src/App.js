2a9a7478f9ea7bcf7e4555e5c279962344196580
Can Guler <canmuhammetguler@gmail.com>
2020-10-08 20:01:26 -0500

    adding new features react-admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
+import restProvider from "ra-data-simple-rest";
+import PostLists from "./components/PostLists";
+import PostCreate from "./components/PostCreate";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Admin dataProvider={restProvider("http://localhost:3000")}>
+      <Resource name="posts" list={PostLists} />
+    </Admin>
+  );
 }
 
 export default App;
---
+++ client/src/components/PostCreate.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Create, SimpleForm, TextInput, DateInput } from "react-admin";
+
+const PostCreate = () => {
+  return <div></div>;
+};
+
+export default PostCreate;
